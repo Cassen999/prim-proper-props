6b4565be3480c0b2b23bd7e54899bb511a148b6f
Cassen Gerber <cassenpt@gmail.com>
2020-12-02 14:27:04 -0600

    base goals done

---
+++ src/components/GuestList/GuestList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class GuestList extends Component{
+    render() {
+        return(
+            <div> 
+                <h2>Guest List</h2>   
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Kids Meal</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.guests.map(guest => (
+                        <tr key={guest.name}>
+                        <td>{guest.name}</td>
+                        <td>{guest.kidsMeal}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default GuestList
